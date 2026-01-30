d4a867fe8379b28719f9c17002af30227555ae9d
Stéphane Bouret <sbouret@orange.fr>
2026-01-30 11:56:38 +0100

    Mise en place de l'enregistrement d'un utilisateur

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -2,6 +2,8 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 import ThemeController from './controllers/theme_controller.js';
 import ResetPassword from './controllers/reset_password_controller.js';
 import DeleteAccountController from './controllers/delete_account_controller.js';
+import AddressAutocompleteController from './controllers/address_autocomplete_controller.js';
+import RegisterController from './controllers/register_controller.js';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
@@ -9,3 +11,5 @@ const app = startStimulusApp();
 app.register('theme', ThemeController);
 app.register('reset-password', ResetPassword);
 app.register('delete-account', DeleteAccountController);
+app.register('address-autocomplete', AddressAutocompleteController);
+app.register('register', RegisterController);
